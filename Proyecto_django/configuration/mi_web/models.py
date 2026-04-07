7fcd2ed7be4b99e538bca172c35c3b9efda0da5a
Victor Sepulveda <starlin2404@gmail.com>
2026-04-07 12:53:06 -0400

    Feat: correccion del apartado de notificas y creacion de post de noticias para que no se vea vacio

--- Proyecto_django/configuration/mi_web/models.py
+++ Proyecto_django/configuration/mi_web/models.py
@@ -9,3 +9,16 @@ class Articulo(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Noticia(models.Model):
+    titulo = models.CharField(max_length=200)
+    resumen = models.TextField(help_text="Un breve resumen de la noticia")
+    contenido = models.TextField()
+    imagen_url = models.URLField(max_length=500, blank=True, null=True)
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+
+    class Meta:
+        verbose_name_plural = "Noticias"
